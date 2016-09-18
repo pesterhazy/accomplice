ebe1de0b89b50ff5332c92c935fd6e71f96d8f22
Paulus Esterhazy <pesterhazy@gmail.com>
2016-09-18 16:49:23 +0200

    Pub/sub

--- src/accomplice/core.clj
+++ src/accomplice/core.clj
@@ -1,26 +1,47 @@
 (ns accomplice.core
-  (:require [clojure.core.async :refer [chan >!! <!! go-loop <!]])
+  (:require [clojure.core.async :refer [chan >!! <!!
+                                        go-loop <!
+                                        pub sub unsub]])
   (:gen-class))
+
+(defn topic [v]
+  :all)
 
 (defprotocol ILog
   (append! [_ event])
   (follow [_]))
 
-(defrecord Log [events c]
+(defrecord Log [events c publication]
   ILog
   (append! [_ event]
     (swap! events conj event)
     (>!! c event))
   (follow [_]
-    (loop []
-      (let [x (<!! c)]
-        (prn x))
-      (recur))))
+    (let [subscriber (chan)]
+      (sub publication :all subscriber)
+      (try
+        (loop []
+          (let [v (<!! subscriber)]
+            (prn v))
+          (recur))
+        (finally
+          (println "Cleaning up...")
+          (unsub publication :all subscriber))))))
 
 (defn make-log []
-  (->Log (atom []) (chan 10)))
+  (let [c (chan)
+        publication (pub c topic)]
+    (->Log (atom []) c publication)))
 
 (def !log (make-log))
+
+(defn annoy []
+  (loop [n 0]
+    (println "Appending..." n)
+    (append! !log n)
+    (println "Sleeping...")
+    (Thread/sleep 1000)
+    (recur (inc n))))
 
 (defn -main
   "I don't do a whole lot ... yet."
